b17d47aa042af91b2ed06c1913cd97fe8d213c62
Le Viet (FMI) <viet.le@fmi.fi>
2022-12-13 13:56:40 +0200

    remove long ground period

--- summary.py
+++ summary.py
@@ -3,14 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 %matplotlib qt
+
 # %%
 save_path = r'C:\Users\le\OneDrive - Ilmatieteen laitos\Campaigns\Pace2022\FMI balloon payload\Processed_data/'
-glob.glob(save_path + '*.csv')
-# %%
 bme = pd.read_csv(save_path + 'bme.csv')
 cpc = pd.read_csv(save_path + 'cpc.csv')
 minicda = pd.read_csv(save_path + 'minicda.csv')
 POPS = pd.read_csv(save_path + 'POPS.csv')
+
+# %%
+flight_time = pd.read_csv(
+    r'C:\Users\le\OneDrive - Ilmatieteen laitos\Campaigns\Pace2022\FMI balloon payload\Raw_data/ground_time.csv')
+flight_time['start'] = pd.to_datetime(flight_time['start'])
+flight_time['end'] = pd.to_datetime(flight_time['end'])
+
+# %%
+POPS['datetime'] = pd.to_datetime(POPS['datetime'])
+bme['datetime'] = pd.to_datetime(bme['datetime'])
+minicda['datetime'] = pd.to_datetime(minicda['datetime'])
+cpc['datetime'] = pd.to_datetime(cpc['datetime'])
 
 # %%
 bme['bme_data'] = True
@@ -26,13 +37,12 @@ df = df.sort_values('datetime')
 df = df.reset_index(drop=True)
 df['datetime'] = pd.to_datetime(df['datetime'])
 
-# %%
+for i, row in flight_time.iterrows():
+    condition = df[(df['flight_ID'] == i+1) &
+                   ((df['datetime'] < row['start']) | (df['datetime'] > row['end']))]
+    df = df.drop(condition.index)
+df = df.reset_index(drop=True)
 grp = df.groupby('flight_ID')
-grp['bme_data'].sum()
-grp['cpc_data'].sum()
-grp['minicda_data'].sum()
-grp['POPS_data'].sum()
-grp['datetime'].first()
 
 # %%
 fig, ax = plt.subplots(figsize=(16, 9))
@@ -52,3 +62,4 @@ ax.set_xlabel('Starting time (UTC+2)')
 ax.set_ylabel('Hours of data')
 ax.grid()
 fig.tight_layout()
+fig.savefig(save_path + 'available_data_balloon.png', dpi=500, bbox_inches='tight')
