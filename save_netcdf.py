47c5aab80909cc20f6a19980711ba20a8686e40e
Le Viet (FMI) <viet.le@fmi.fi>
2026-01-30 16:09:18 +0200

    new version

--- save_netcdf.py
+++ save_netcdf.py
@@ -1,12 +1,18 @@
 import xarray as xr
 import glob
 import pandas as pd
-from UAVision.preprocess import pops_binedges, calculate_midbin, mcda_midbin_all
+# from UAVision.preprocess import pops_binedges, calculate_midbin, mcda_midbin_all
+import numpy as np
+import json
 import re
 
 csv_dir = r'C:\Users\le\OneDrive - Ilmatieteen laitos\Campaigns\Pace2022\FMI balloon payload\Processed_data/'
 csv_files = glob.glob(csv_dir + '*.csv')
-pops_midbin = calculate_midbin(pops_binedges)
+pops_binedges = np.loadtxt('pops_binedges.txt')
+pops_midbin = (pops_binedges[1:] + pops_binedges[:-1])/2
+
+with open('mcda_midbin_all.txt', 'r') as file: 
+    mcda_midbin_all = json.loads(file.read())
 for file in csv_files:
     df = pd.read_csv(file)
     df['datetime (utc)'] = pd.to_datetime(df['datetime (utc)'])
@@ -21,7 +27,7 @@ for file in csv_files:
         mcda_midbin = mcda_midbin_all[size]
         mcda_midbin = mcda_midbin[81:]
 
-    if df['datetime'][0] < pd.Timestamp('20220924'):
+    if df['datetime'][0] < pd.Timestamp('20220929'):
         platform = "Helikites"
     else:
         platform = "Aerostat"
@@ -45,18 +51,18 @@ for file in csv_files:
     df_xr = df_xr[['temp_bme (C)',
                     'press_bme (hPa)',
                     'rh_bme (%)',
+                    'height_bme (m)',
                     'N_conc_cpc (cm-3)',
                     'press_cpc (hPa)',
                     'N_conc_pops (cm-3)',
                     'press_pops (hPa)',
                     'flow_rate_pops (l/m)',
+                    'temp_pops (C)',
+                    'rh_pops (%)',
                     'conc_pops (cm-3)',
                     'conc_pops_dNdlogDp',
                     'conc_mcda (cm-3)',
                     'conc_mcda_dNdlogDp',
-                    'pm1_mcda',
-                    'pm25_mcda',
-                    'pm10_mcda',
                     'Nd_mcda (cm-3)',
                     'LWC_mcda (g/m3)',
                     'MVD_mcda (um)',
@@ -69,18 +75,18 @@ for file in csv_files:
     df_xr['temp_bme'].attrs = {'units': 'degree Celsius', 'long_name': 'Temperature measured by BME280', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['press_bme'].attrs = {'units': 'hPa', 'long_name': 'Pressure measured by BME280', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['rh_bme'].attrs = {'units': '%', 'long_name': 'Relative humidity measured by BME280', '_FillValue': -9999.9, 'processing_level': 'b1'}
+    df_xr['height_bme'].attrs = {'units': 'm', 'long_name': 'Height above ground level calculated from BME280', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['N_conc_cpc'].attrs = {'units': 'cm-3', 'long_name': 'Total particle concentration measured by CPC', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['press_cpc'].attrs = {'units': 'hPa', 'long_name': 'Inlet pressure measured by CPC', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['N_conc_pops'].attrs = {'units': 'cm-3', 'long_name': 'Total particle concentration measured by POPS', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['press_pops'].attrs = {'units': 'hPa', 'long_name': 'Inlet pressure measured by POPS', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['flow_rate_pops'].attrs = {'units': 'L/m', 'long_name': 'Inlet flow rate measured by POPS', '_FillValue': -9999.9, 'processing_level': 'b1'}
+    df_xr['temp_pops'].attrs = {'units': 'degree Celsius', 'long_name': 'Internal temperature measured by POPS', '_FillValue': -9999.9, 'processing_level': 'b1'}
+    df_xr['rh_pops'].attrs = {'units': '%', 'long_name': 'Internal relative humidity calculated for POPS', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['conc_pops'].attrs = {'units': 'cm-3', 'long_name': 'Particle concentration measured by POPS', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['conc_pops_dNdlogDp'].attrs = {'units': 'cm-3', 'long_name': 'Normalized particle concentration measured by POPS in dN/dlogDp', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['conc_mcda'].attrs = {'units': 'cm-3', 'long_name': 'Particle concentration measured by mCDA', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['conc_mcda_dNdlogDp'].attrs = {'units': 'cm-3', 'long_name': 'Particle concentration measured by mCDA in dN/dlogDp', '_FillValue': -9999.9, 'processing_level': 'b1'}
-    df_xr['pm1_mcda'].attrs = {'units': 'µg cm−3', 'long_name': 'Particulate Matter with diameters less than 1µm measured by mCDA', '_FillValue': -9999.9, 'processing_level': 'b1'}
-    df_xr['pm25_mcda'].attrs = {'units': 'µg cm−3', 'long_name': 'Particulate Matter with diameters less than 2.5µm measured by mCDA', '_FillValue': -9999.9, 'processing_level': 'b1'}
-    df_xr['pm10_mcda'].attrs = {'units': 'µg cm−3', 'long_name': 'Particulate Matter with diameters less than 10µm measured by mCDA', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['Nd_mcda'].attrs = {'units': 'cm-3', 'long_name': 'Total cloud droplet concentration measured by mCDA', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['LWC_mcda'].attrs = {'units': 'g m−3', 'long_name': 'Liquid water content measured by mCDA', '_FillValue': -9999.9, 'processing_level': 'b1'}
     df_xr['MVD_mcda'].attrs = {'units': 'µm', 'long_name': 'Median volume diameter measured by mCDA', '_FillValue': -9999.9, 'processing_level': 'b1'}
